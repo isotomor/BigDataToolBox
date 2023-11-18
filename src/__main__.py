faf50e0cf8587e989d0c63fb91d691a242285bda
Nacho <i.sotomoreno@gmail.com>
2023-11-18 10:49:01 +0100

    Actualizamos...

--- src/__main__.py
+++ src/__main__.py
@@ -3,32 +3,35 @@ Script main de ejecución que contiene el orquestador de los distintos ficheros 
 """
 import sys
 
-from .init_config import launcher
-from .run_tablon import run_tablon
-from .models.main_baseline_prophet_cval import run_main_baseline_prophet_cval
+from src.init_config import launcher
+from src.run_tablon import run_tablon
+from src.run_tablon_analytics import run_tablon_analytics
+from src.models.main_baseline_prophet_cval import run_main_baseline_prophet_cval
+
 
 JOBS = dict(
     JOB_RUN_TABLON=dict(function=run_tablon, init_spark=True, use_databricks_spark=True),
-    JOB_PROPHET_CVAL=dict(function=run_main_baseline_prophet_cval, init_spark=True, use_databricks_spark=True)
+    JOB_PROPHET_CVAL=dict(function=run_main_baseline_prophet_cval, init_spark=True, use_databricks_spark=True),
+    JOB_RUN_TABLON_ANALYTICS=dict(function=run_tablon_analytics, init_spark=True, use_databricks_spark=True)
 )
 
 
-def main(function_main, enviroment_main):
+def main():
+    print("Iniciando proceso...")
+    # Obtenemos el nombre de la función como parámetro de entrada
+    job_function = sys.argv[1]
+    enviroment = sys.argv[2] if len(sys.argv) > 2 else 'DESARROLLO'
+
     try:
-        function = JOBS.get(function_main)
+        function = JOBS.get(job_function)
     except IndexError:
         raise Exception("Es necesario el argumento de la función a ejecutar")
 
     # Lanzamos la ejecución de la función.
     launcher(func=function["function"], init_spark=function["init_spark"],
              use_databricks_spark=function["use_databricks_spark"],
-             enviroment=enviroment_main)
+             enviroment=enviroment)
 
 
 if __name__ == "__main__":
-
-    # Obtenemos el nombre de la función como parámetro de entrada
-    job_function = sys.argv[1]
-    enviroment = sys.argv[2] if len(sys.argv) > 2 else 'DESARROLLO'
-
-    main(function_main=job_function, enviroment_main=enviroment)
+    main()
